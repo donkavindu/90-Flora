aa5b9ba310d70295280f829d4cf734cd5d47f6c5
Kavindu0717 <donkavindulakshan@gmail.com>
2025-07-14 09:50:38 +0530

    Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: "90 Flora",
   description: "Luxury Floral Designs for Weddings & Events",
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
