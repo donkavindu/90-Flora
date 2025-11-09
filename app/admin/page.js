f2d84574f1d98d57ef7fc4684fcdebcc9ffe6b1d
Kavindu0717 <donkavindulakshan@gmail.com>
2025-11-09 12:37:13 +0530

    admin passwors css

--- app/admin/page.js
+++ app/admin/page.js
@@ -76,7 +76,7 @@ export default function AdminPage() {
                   name="password"
                   type="password"
                   required
-                  className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  className="block w-full px-3 py-2 mt-1 tracking-[2px] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
